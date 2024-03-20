a16b91ee6ca01e6238a74a19bb4e484b177ed1c5
Joe Hanes <jhanes@godaddy.com>
2024-03-20 13:01:23 -0700

    appEnv fix

--- lifecycles/appEnvConfig.js
+++ lifecycles/appEnvConfig.js
@@ -1,5 +1,3 @@
-const { merge } = require('lodash');
-
 const logPrefix = 'lifecycle:appEnvConfig';
 
 module.exports = async function (gasket, config) {
@@ -10,9 +8,5 @@ module.exports = async function (gasket, config) {
     const { logger } = gasket;
     logger.debug(`${logPrefix}: Using environment configuration for NODE_ENV=${process.env.NODE_ENV}`);
 
-    if (!envConfig) {
-        throw new Error(`${logPrefix} No environment configuration found for NODE_ENV=${process.env.NODE_ENV}`);
-    }
-
-    return merge(config, envConfig);
+    return config;
 }
