9235529a7644c3a21883745bf647d8224254a118
Joe Hanes <jhanes@godaddy.com>
2024-09-05 13:29:26 -0700

    lexical

--- pages/lexical-search.js
+++ pages/lexical-search.js
@@ -1,17 +1,19 @@
-import React, { useEffect, useState, useMemo } from 'react';
+import React, { useEffect, useState } from 'react';
 import Box from '@ux/box';
 import text from '@ux/text';
 import Head from '../components/head';
-import dynamic from 'next/dynamic';
-import SiblingSet from '@ux/sibling-set';
 import Card from '@ux/card';
-import Button from '@ux/button';
+import SiblingSet from '@ux/sibling-set';
 import TextInput from '@ux/text-input';
 import SelectInput from '@ux/select-input';
-import Tabs from '@ux/tabs';
+import Button from '@ux/button';
 import { CodeBlock, CopyBlock, Code, dracula } from 'react-code-blocks';
+import Table, { Thead, Tbody, Tfoot, Th, Td, Tr, SortableHeader } from '@ux/table';
+import '@ux/table/styles';
+import example from '../lib/lexical-search/example-2.json'
+import dynamic from 'next/dynamic';
+
 const QueryBoolEditor = dynamic(() => import('../components/query-bool-editor'), { ssr: false });
-import * as example2 from '../lib/lexical-search/example-2.json'
 
 const headerText = `In OpenSearch, you typically use the bool query to combine multiple 
 conditions using must, should, and must_not.`;
@@ -21,35 +23,45 @@ specific distance between words, you use the match_phrase query with the slop pa
 must: All conditions must be true (AND logic).
 should: At least one of the conditions must be true (OR logic).
 must_not: None of the conditions should be true (NOT logic).`;
-function MyTabs() {
-  const config = [{
-    id: 'criteria',
-    text: 'Criteria',
-    content: 'QueryBoolEditor'
-  }, {
-    id: 'query',
-    text: 'Query',
-    content: 'Code'
-  }];
-  const items = useMemo(() => config, [config]);
-  const [active, setActive] = useState(items[0].id);
-  const id = 'my-tabs';
 
-  return (
-    <>
-      {/* Tabs component */}
-      <Tabs id={id} items={items} onChange={setActive} />
-
-      {/* User provided panel */}
-      <div {...Tabs.panelAttrs(id, active)}>{items.find(item => item.id === active).content}</div>
-    </>
-  );
-}
 const LexicalSearch = () => {
   const [isClient, setIsClient] = useState(false);
+  const [criteria, setCriteria] = useState([]);
+  const [field, setField] = useState('');
+  const [clause, setClause] = useState('must');
+  const [type, setType] = useState('match');
+  const [queryType, setQueryType] = useState('match_phrase');
+  const [queryText, setQueryText] = useState('');
+  const [query, setQuery] = useState('');
   useEffect(() => {
+    console.log(example);
     setIsClient(true);
   }, []);
+
+  const addCriteria = () => {
+    setCriteria([...criteria, { field, operator, type }]);
+    setField('');
+    setOperator('must');
+    setType('match');
+  };
+
+  const buildBoolQuery = () => {
+    const boolQuery = {
+      bool: {
+        must: [],
+        should: [],
+        must_not: []
+      }
+    };
+
+    criteria.forEach(({ field, operator, type }) => {
+      const query = { [type]: { [field]: '' } };
+      boolQuery.bool[operator].push(query);
+    });
+
+    return boolQuery;
+  };
+
   return (
     <>
       <Head title='Lexical Search' description='Lexical Search' route='search' />
@@ -58,32 +70,102 @@ const LexicalSearch = () => {
         <text.p as='paragraph' text={headerText} />
         <text.p as='paragraph' text={otherText} />
 
+        <Box orientation='horizontal' blockAlignChildren='start' >
 
-        <Box orientation='horizontal' space={{ block: true, inline: true }} >
           <Card id='criteria-input'>
-            <SiblingSet stretch gap='sm' size='sm'>
-              <TextInput visualSize='sm' id='field' label='Field' />
-              <SelectInput visualSize='sm' id='operator' label='Operator' >
-                <option value='must'>must</option>
-                <option value='should'>should</option>
-                <option value='must_not'>must_not</option>
-              </SelectInput>
-              <SelectInput visualSize='sm' id='type' label='Type' >
-                <option value='match'>match</option>
-                <option value='match_phrase'>match_phrase</option>
-              </SelectInput>
-              <Button id='add' display='inline' text='Add' />
+            <Box gap='md' blockPadding='md'>
+              <SiblingSet stretch gap='sm' size='sm'>
 
-            </SiblingSet>
+                <SelectInput
+                  visualSize='sm'
+                  id='clause'
+                  label='Clause'
+                  value={clause}
+                  onChange={(e) => setClause(e)}
+                >
+                  <option value='must'>must</option>
+                  <option value='should'>should</option>
+                  <option value='must_not'>must_not</option>
+                </SelectInput>
+                <SelectInput
+                  visualSize='sm'
+                  id='queryType'
+                  label='Query Type'
+                  value={queryType}
+                  onChange={(e) => setQueryType(e)}
+                >
+                  <option value='match_phrase'>match_phrase</option>
+                </SelectInput>
+                <SelectInput
+                  visualSize='sm'
+                  id='query'
+                  label='Query'
+                  value={query}
+                  onChange={(e) => setQuery(e)}
+                >
+                  <option value='customer_conversation'>customer_conversation</option>
+                  <option value='agent_conversation'>agent_conversation</option>
+                </SelectInput>
+                <TextInput style={{ 'minWidth': '250px', 'width': '100%' }} visualSize='sm' id='queryText' label='Text' value={queryText} onChange={(e) => setQueryText(e)} />
+                <Button id='add' design='inline' text='Add' onClick={addCriteria} />
+              </SiblingSet>
+            </Box>
           </Card>
-          <Card id='code'>
-            <Box orientation='vertical' space={{ block: true, inline: true }}>
-              <CodeBlock text={JSON.stringify(example2.default)} language='javascript' showLineNumbers={true} wrapLongLines={true} />
+          <Box>
+            <Table className='example' density='sm'>
+              <Thead>
+                <Tr>
+                  <Th scope='col'>Person</Th>
+                  <Th scope='col'>Interest</Th>
+                  <Th scope='col'>UID</Th>
+                  <Th scope='col'>Age</Th>
+                </Tr>
+              </Thead>
+              <Tbody>
+                <Tr>
+                  <Th scope='row'>Chris</Th>
+                  <Td>HTML tables</Td>
+                  <Td>12345</Td>
+                  <Td>33</Td>
+                </Tr>
+                <Tr>
+                  <Th scope='row'>Amanda</Th>
+                  <Td>React</Td>
+                  <Td>12345</Td>
+                  <Td>33</Td>
+                </Tr>
+                <Tr>
+                  <Th scope='row'>Sarah</Th>
+                  <Td>CSS</Td>
+                  <Td>12345</Td>
+                  <Td>33</Td>
+                </Tr>
+                <Tr>
+                  <Th scope='row'>Steve</Th>
+                  <Td>HTML buttons</Td>
+                  <Td>12345</Td>
+                  <Td>33</Td>
+                </Tr>
+              </Tbody>
+              <Tfoot>
+                <Tr>
+                  <Th scope='row' colSpan='3'>Average age</Th>
+                  <Td>33</Td>
+                </Tr>
+              </Tfoot>
+            </Table>
 
-            </Box></Card>
-        </Box>
+          </Box>
+          <Box>
+            <Card id='code'>
+              <CodeBlock className='code-block-custom-styles' text={JSON.stringify(example, null, 2)} language='json' showLineNumbers={true} wrapLongLines={true} /></Card>
+            {/* <Card stretch id='query-bool-editor'>
+            {isClient && <QueryBoolEditor options={{ mode: 'code' }} value={buildBoolQuery()} />}
+          </Card> */}
+          </Box></Box>
       </Box>
     </>
   );
 }
+
 export default LexicalSearch;
