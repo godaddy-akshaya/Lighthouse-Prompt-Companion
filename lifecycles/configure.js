011295f44c52d1d21a80b2f5e3219fb0ab70f8b4
Hanezz <jhanes@godaddy.com>
2024-12-02 11:34:48 -0700

    pkg updates

--- lifecycles/configure.js
+++ lifecycles/configure.js
@@ -1,12 +1,9 @@
 const { createCertificate } = require("@godaddy/quickcert");
 
 module.exports = async function (gasket, config) {
-  console.log('configuring server');
-  console.log(process.env.ECS_TLS);
   if (process.env.ECS_TLS) {
     const { cert, key } = await createCertificate();
     config.https = { port: 8443, cert, key };
   }
-
   return config;
 };
