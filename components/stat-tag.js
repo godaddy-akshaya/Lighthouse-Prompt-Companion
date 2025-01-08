195a487b876474f6c755444dfa61efb0b1526800
Joe Hanes <jhanes@godaddy.com>
2025-01-08 15:12:57 -0700

    Get hits component

--- components/stat-tag.js
+++ components/stat-tag.js
@@ -1,19 +1,27 @@
 import React from 'react';
 import Box from '@ux/box';
-import Card, { spaceOptions } from '@ux/card';
+import Card from '@ux/card';
 import text from '@ux/text';
-import Tag from '@ux/tag';
 
-const StatTag = ({ title, count, total, percentage }) => {
-    return (
-        <Box orientation='horizontal' blockAlignChildren='start' inlineAlignChildren='start'>
-            <Card id={title} space={{ inline: 'sm', block: 'sm' }}>
-                <text.label as='label' text={`${title} ${percentage}`} />
-                <Box gap={'sm'}>
-                    <text.span as='caption' text={`${count} of ${total}`} />
-                </Box>
-            </Card>
-        </Box>
-    )
+const StatTag = ({ title, tags }) => {
+  const formatNumber = (number) => {
+    // check if has decimal
+    if (number % 1 !== 0) {
+      return number.toFixed(2);
+    } else {
+      return number.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ","); // add comma to number
+    }
+  }
+  return (
+    <Card space={{ block: 'sm', inline: 'sm' }}>
+      <text.label text={title?.toString().toUpperCase()} as='label' /><br />
+      <Box>
+        {tags.map((tag, index) => (
+          <Box key={index}>
+            {`${tag.name.split('_').join(' ')} ${formatNumber(tag.value)}`}
+          </Box>
+        ))} </Box>
+    </Card>
+  )
 }
 export default StatTag;
