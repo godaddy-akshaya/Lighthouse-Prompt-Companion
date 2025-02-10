8eb207a2c7669185eef2cea21059a86545766fea
Joe Hanes <jhanes@godaddy.com>
2025-02-10 15:46:59 -0700

    removed test integer

--- pages/table/[...id].js
+++ pages/table/[...id].js
@@ -97,7 +97,7 @@ const PromptBuilder = ({ authDetails }) => {
           setShowUserMessage(true);
           setShowMessage(false);
         } else {
-          setNumOfTransactions(10 || 10);
+          setNumOfTransactions(data || 0);
           setShowMessage(false);
         }
         setIsPromptLoading(false);
