57f0c522e89340355dee7d5341a948f061c3e0c1
Joe Hanes <jhanes@godaddy.com>
2025-03-05 09:01:45 -0700

    Feature: Added Dashboard dropdown

--- pages/quicksight/index.js
+++ pages/quicksight/index.js
@@ -5,6 +5,7 @@ import Text from '@ux/text';  // Capitalized
 import Head from '../../components/head';
 import Card from '@ux/card';
 import Spinner from '@ux/spinner';
+import SelectInput from '@ux/select-input';  
 import QuickSightEmbed from '../../components/quicksight/quicksight-embed';  // Fixed import path
 import { BannerMessage } from '../../components/banner-message';
 const getEmbedUrl$ = async (id) => {
@@ -21,10 +22,19 @@ e43656f2-ad59-454a-8825-5e7c0effb3ab - Intent Insights
 
 const QuickSightPage = () => {
   const [loading, setLoading] = useState(false);
-  const [dashboardId, setDashboardId] = useState('ec5da3b7-a5d8-4685-a334-6e14381daca9');
+  const [dashboardId, setDashboardId] = useState();
   const [dashboardUrl, setDashboardUrl] = useState(null);
   const [error, setError] = useState(null);
-
+  const [dashbaords, setDashboards] = useState([{
+    name: 'Lighthouse Offer Dashboard',
+    id: 'ec5da3b7-a5d8-4685-a334-6e14381daca9'
+  },{
+    name: 'Contact Driver',
+    id: '35ba3d06-ed89-499a-8d1b-5176205eee64'
+  },{
+    name: 'Intent Insights',
+    id: 'e43656f2-ad59-454a-8825-5e7c0effb3ab'
+  }]);
   useEffect(() => {
     console.log('QuickSightPage mounted');
     const fetchDashboardUrl = async () => {
@@ -57,6 +67,14 @@ const QuickSightPage = () => {
       </Box>
       <Box gap='md' blockPadding='md' inlinePadding='md'>
         {error && <BannerMessage showMessage={true} message={error} userMessageType="error" handleCloseError={() => setError(null)} />}
+        <Box orientation="horizontal" gap="md">
+            <SelectInput id='dashboard-select' label='Select Dashboard' options={dashbaords} onChange={(e) => setDashboardId(e)}>
+              {dashbaords.map((d) => {
+                return <option key={d.id} value={d.id}>{d.name}</option>;
+              })}
+            </SelectInput>
+
+          </Box>
         <Card id="quicksight-card" className="quicksight-card">
           {loading && <Spinner size="lg" />}  {/* Fixed variable */}
           {dashboardId && dashboardUrl && (
