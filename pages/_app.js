fb98fe5b1b182d5c78ae1265201d96f64d95c351
Joe Hanes <85644333+jhanes-godaddy@users.noreply.github.com>
2025-07-27 10:16:36 -0700

    Redirect fix (#207)
    * added authRequired to initial load
    
    * session fix
    
    * manual deploy

--- pages/_app.js
+++ pages/_app.js
@@ -1,22 +1,31 @@
-import '../styles/global.scss';
-import { withAuthRequired, withAuthProvider } from '@godaddy/gasket-auth';
-import { createApp, reportWebVitals } from '@godaddy/gasket-next';
-import gasket from '../gasket';
+import "../styles/global.scss";
+import { withAuthRequired, withAuthProvider } from "@godaddy/gasket-auth";
+import {
+  createApp,
+  reportWebVitals,
+  withPageEnhancers,
+} from "@godaddy/gasket-next";
+import gasket from "../gasket";
+
+/* auth */
+function customSubdomain(host) {
+  const parts = host.split(":")[0].split(".").slice(0, -2);
+  if (parts.length >= 3) return parts.join(".");
+}
 
 function Layout(props) {
   const { Component, pageProps } = props;
 
-   return (
-     <Component { ...pageProps } />
-   );
- }
+  return <Component {...pageProps} />;
+}
 
 const App = createApp({ Layout, initialProps: true });
+const authRequired = withAuthRequired({
+  realm: "jomax",
+  ssoRedirectSubdomain: customSubdomain,
+  gasket,
+});
 
-export default [
-  withAuthProvider(),
-].reduce((cmp, hoc) => hoc(cmp), App);
+export default withAuthProvider()(withPageEnhancers([authRequired])(App));
 
 export { reportWebVitals };
-
-
--- pages/table/[...id].js
+++ pages/table/[...id].js
@@ -1,23 +1,27 @@
-
-import React, { useEffect, useState } from 'react';
-import { useRouter } from 'next/router';
-import Spinner from '@ux/spinner';
-import Box from '@ux/box';
-import Head from '../../components/head';
-import text from '@ux/text';
-import Button from '@ux/button';
-import Card from '@ux/card';
-import TableSelect from '../../components/table-select';
-import { submitRowCountRequest, getTableFilters, submitPromptJob } from '../../lib/data/data.service';
-import Alert from '@ux/alert';
-import session from '../../lib/session.js';
-import { getGuid } from '../../lib/utils';
-import MessageOverlay from '@ux/message-overlay';
-import TableFilter from '../../components/table-filter';
-import PromptForm from '../../components/prompt-form';
-import TwoColumnLayout from '../../components/layout/two-column-layout';
+import React, { useEffect, useState } from "react";
+import { useRouter } from "next/router";
+import Spinner from "@ux/spinner";
+import Box from "@ux/box";
+import Head from "../../components/head";
+import text from "@ux/text";
+import Button from "@ux/button";
+import Card from "@ux/card";
+import TableSelect from "../../components/table-select";
+import {
+  submitRowCountRequest,
+  getTableFilters,
+  submitPromptJob,
+} from "../../lib/data/data.service";
+import Alert from "@ux/alert";
+import session from "../../lib/session.js";
+import { getGuid } from "../../lib/utils";
+import MessageOverlay from "@ux/message-overlay";
+import TableFilter from "../../components/table-filter";
+import PromptForm from "../../components/prompt-form";
+import TwoColumnLayout from "../../components/layout/two-column-layout";
 
 const PromptBuilder = ({ authDetails }) => {
+  console.log(authDetails);
   const [isLoading, setIsLoading] = useState(true);
   const [showMessage, setShowMessage] = useState(false);
   const [numOfTransactions, setNumOfTransactions] = useState(null);
@@ -28,22 +32,24 @@ const PromptBuilder = ({ authDetails }) => {
   const [filters, setFilters] = useState();
 
   const [jobModel, setJobModel] = useState({
-    prompt: '',
+    prompt: "",
     evaluation: false,
-    model: '',
-    evaluation_model: '',
-    evaluation_prompt: '',
-    evaluation_provider: '',
+    model: "",
+    evaluation_model: "",
+    evaluation_prompt: "",
+    evaluation_provider: "",
     filterOptions: [],
-    extras: []
+    extras: [],
   });
-  const [errorMessage, setErrorMessage] = useState('Something went wrong');
+  const [errorMessage, setErrorMessage] = useState("Something went wrong");
   const router = useRouter();
   const [routeParams, setRouteParams] = useState({
-    table: decodeURIComponent(router.query?.id?.[0] || '0'),
-    display_name: decodeURI(router.query?.display_name) || decodeURIComponent(router.query?.id?.[0] || '0')
+    table: decodeURIComponent(router.query?.id?.[0] || "0"),
+    display_name:
+      decodeURI(router.query?.display_name) ||
+      decodeURIComponent(router.query?.id?.[0] || "0"),
   });
-  if (authDetails) session.setSessionItem('weblogin', authDetails.accountName);
+  if (authDetails) session.setSessionItem("weblogin", authDetails.accountName);
 
   function handleOnSubmit(formValues) {
     setIsLoading(true);
@@ -56,30 +62,38 @@ const PromptBuilder = ({ authDetails }) => {
         evaluation: formValues.includeEval,
         model: formValues.promptModel,
         evaluation_model: formValues.evaluationModel || null,
-        evaluation_prompt: formValues.evaluationPrompt || null
+        evaluation_prompt: formValues.evaluationPrompt || null,
       };
 
-      submitPromptJob(routeParams.table, job, jobModel.filterOptions, jobModel.extras).then((data) => {
-
-        if (data?.errorMessage) {
-          setErrorMessage(data.errorMessage);
+      submitPromptJob(
+        routeParams.table,
+        job,
+        jobModel.filterOptions,
+        jobModel.extras
+      ).then(
+        (data) => {
+          if (data?.errorMessage) {
+            setErrorMessage(data.errorMessage);
+            setIsLoading(false);
+            setShowUserMessage(true);
+          } else {
+            router.push(`/run-status?newJob=${g}`, undefined, {
+              shallow: true,
+            });
+          }
+        },
+        (error) => {
+          console.log("error", error);
+          setErrorMessage(error.toString());
           setIsLoading(false);
           setShowUserMessage(true);
-        } else {
-          router.push(`/run-status?newJob=${g}`, undefined, { shallow: true });
         }
-
-      }, error => {
-        console.log('error', error);
-        setErrorMessage(error.toString());
-        setIsLoading(false);
-        setShowUserMessage(true);
-      });
+      );
     })();
   }
   function handleCloseError(e) {
     setShowUserMessage(false);
-    setErrorMessage('');
+    setErrorMessage("");
   }
   const handleError = (error) => {
     setErrorMessage(error);
@@ -95,106 +109,142 @@ const PromptBuilder = ({ authDetails }) => {
     setShowMessage(false);
     setNumOfTransactions(null);
     setTimeout(() => {
-      window.scrollTo({ top: 0, behavior: 'smooth' });
+      window.scrollTo({ top: 0, behavior: "smooth" });
     }, 300);
     try {
       setJobModel({ ...jobModel, filterOptions, extras });
-      submitRowCountRequest(routeParams.table, filterOptions, extras).then(data => {
-        if (data?.errorMessage) {
-          setNumOfTransactions(0);
-          setErrorMessage(data.errorMessage);
+      submitRowCountRequest(routeParams.table, filterOptions, extras).then(
+        (data) => {
+          if (data?.errorMessage) {
+            setNumOfTransactions(0);
+            setErrorMessage(data.errorMessage);
+            setShowUserMessage(true);
+            setShowMessage(false);
+          } else {
+            setNumOfTransactions(data || 0);
+            // used in testing  setNumOfTransactions(10);
+            setShowMessage(false);
+          }
+          setIsPromptLoading(false);
+        },
+        (error) => {
+          setErrorMessage(error);
+          setIsLoading(false);
           setShowUserMessage(true);
-          setShowMessage(false);
-        } else {
-          setNumOfTransactions(data || 0);
-          // used in testing  setNumOfTransactions(10);
-          setShowMessage(false);
         }
-        setIsPromptLoading(false);
-      }, error => {
-        setErrorMessage(error);
-        setIsLoading(false);
-        setShowUserMessage(true);
-      });
-
+      );
     } catch (error) {
       handleError(error);
     }
   };
   useEffect(() => {
-    if (routeParams.table === '0') {
+    if (routeParams.table === "0") {
       setShowTableSelect(true);
       setIsLoading(false);
     } else {
-      getTableFilters(routeParams.table).then(data => {
-        if (data?.errorMessage) {
-          setErrorMessage(data.errorMessage);
-          setShowUserMessage(true);
+      getTableFilters(routeParams.table)
+        .then((data) => {
+          if (data?.errorMessage) {
+            setErrorMessage(data.errorMessage);
+            setShowUserMessage(true);
+            setIsLoading(false);
+            setFilters([]);
+          } else setFilters(data);
+          setShowTableSelect(false);
           setIsLoading(false);
-          setFilters([]);
-        } else
-        setFilters(data);
-        setShowTableSelect(false);
-        setIsLoading(false);
-      }).catch(error => handleError(error));
+        })
+        .catch((error) => handleError(error));
     }
   }, [routeParams]);
   return (
-    <>  <Head title='Prompt Parameters' route='table' />
+    <>
+      {" "}
+      <Head title='Prompt Parameters' route='table' />
       {showTableSelect && <TableSelect />}
-      {showUserMessage &&
+      {showUserMessage && (
         <Box>
           <Alert
             title={errorMessage}
             id='critical-message'
-            emphasis="critical"
-            actions={<Button design="inline" onClick={handleCloseError} text="Close" />} />
+            emphasis='critical'
+            actions={
+              <Button design='inline' onClick={handleCloseError} text='Close' />
+            }
+          />
         </Box>
-      }
-      {isLoading &&
+      )}
+      {isLoading && (
         <Box blockPadding='xl' inlinePadding='xl' inlineAlignChildren='center'>
           <Spinner size='md' />
         </Box>
-      }
-      {
-        !isLoading && !showTableSelect && <>
+      )}
+      {!isLoading && !showTableSelect && (
+        <>
           <Box>
-            <text.h1 text={routeParams.display_name || 'missing'} as='heading' />
+            <text.h1
+              text={routeParams.display_name || "missing"}
+              as='heading'
+            />
             <TwoColumnLayout>
               <Box>
-                <TableFilter filters={filters} onSubmit={handleTableRowSubmit} />
+                <TableFilter
+                  filters={filters}
+                  onSubmit={handleTableRowSubmit}
+                />
               </Box>
-              {isPromptVisible &&
+              {isPromptVisible && (
                 <Box stretch>
                   <text.h3 as='title' text='Parameters' />
-                  {numOfTransactions > 0 &&
-                    <PromptForm onSubmit={handleOnSubmit} numOfTransactions={numOfTransactions} />
-                  }
-                  {numOfTransactions == 0 && <Box>
-                    <Card className='lh-prompt-form-card' id='para-card' stretch title='Parameters' space={{ block: 'lg', inline: 'lg' }}>
-                      <text.h4 as='title' text='No Transactions Found' />
-                      <text.p text='No transactions found based on your selections' />
-                    </Card>
-                  </Box>
-                  }
-                  {isPromptLoading &&
+                  {numOfTransactions > 0 && (
+                    <PromptForm
+                      onSubmit={handleOnSubmit}
+                      numOfTransactions={numOfTransactions}
+                    />
+                  )}
+                  {numOfTransactions == 0 && (
+                    <Box>
+                      <Card
+                        className='lh-prompt-form-card'
+                        id='para-card'
+                        stretch
+                        title='Parameters'
+                        space={{ block: "lg", inline: "lg" }}
+                      >
+                        <text.h4 as='title' text='No Transactions Found' />
+                        <text.p text='No transactions found based on your selections' />
+                      </Card>
+                    </Box>
+                  )}
+                  {isPromptLoading && (
                     <Box stretch>
-                      <Card className='lh-prompt-form-card' stretch id='para-card' title='Parameters'>
-                        <MessageOverlay onEventBehind={handleTableRowSubmit} >
-                          <Box blockPadding='lg' inlinePadding='lg' className='text-center'>
-                            <text.label as='label' text='Getting number of transcripts based on your selections' />
+                      <Card
+                        className='lh-prompt-form-card'
+                        stretch
+                        id='para-card'
+                        title='Parameters'
+                      >
+                        <MessageOverlay onEventBehind={handleTableRowSubmit}>
+                          <Box
+                            blockPadding='lg'
+                            inlinePadding='lg'
+                            className='text-center'
+                          >
+                            <text.label
+                              as='label'
+                              text='Getting number of transcripts based on your selections'
+                            />
                             <Spinner size='md' />
                           </Box>
                         </MessageOverlay>
                       </Card>
                     </Box>
-                  }
+                  )}
                 </Box>
-              }
+              )}
             </TwoColumnLayout>
           </Box>
         </>
-      }
+      )}
     </>
   );
 };
